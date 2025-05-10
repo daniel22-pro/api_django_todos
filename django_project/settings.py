b5351ec1d3ee51c9bc213510d6c87b88edd4e834
Daniel Guerrero <daniguerrero012@gmail.com>
2025-05-10 11:44:00 -0500

    nuevas actualizaciones para la implementacion de heroku

--- django_project/settings.py
+++ django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-r!twgczxz*c0&k+bdk55#xhugfs^b+5eit9kgode8#zoh&_!%u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".herokuapp.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
